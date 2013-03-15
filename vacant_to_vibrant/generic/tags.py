6be7a5841bf3b2309782ac9c4bf21cfb8cc54226
Eric Brelsford <ebrelsford@gmail.com>
2013-03-15 14:13:32 -0400

    refactor tags with generic relations

---
+++ vacant_to_vibrant/generic/tags.py
@@ -0,0 +1,54 @@
+"""
+Helpers for creating template tags for models with generic relations.
+
+"""
+from django.contrib.contenttypes.models import ContentType
+
+from classytags.arguments import Argument
+from classytags.core import Options
+from classytags.helpers import AsTag, InclusionTag
+
+
+class GenericRelationMixin(object):
+
+    def get_objects(self, target):
+        return self.model.objects.filter(
+            content_type=ContentType.objects.get_for_model(target),
+            object_id=target.pk,
+        )
+
+
+class RenderGenericRelationList(GenericRelationMixin, InclusionTag):
+    options = Options(
+        'for',
+        Argument('target', required=True, resolve=True)
+    )
+
+    # TODO unfortunately required
+    template = ''
+
+    def get_context(self, context, target):
+        return {
+            self.get_model_plural_name(): self.get_objects(target),
+        }
+
+    def get_template(self, context, **kwargs):
+        return '%s/%s_list.html' % (
+            self.model._meta.app_label,
+            self.model._meta.object_name.lower(),
+        )
+
+    def get_model_plural_name(self):
+        return self.model._meta.object_name.lower() + 's'
+
+
+class GetGenericRelationList(GenericRelationMixin, AsTag):
+    options = Options(
+        'for',
+        Argument('target', required=True, resolve=True),
+        'as',
+        Argument('varname', required=True, resolve=False),
+    )
+
+    def get_value(self, context, target):
+        return self.get_objects(target)
--- vacant_to_vibrant/photos/templatetags/photos_tags.py
+++ vacant_to_vibrant/photos/templatetags/photos_tags.py
@@ -1,54 +1,21 @@
 """
-Template tags for the photos app, loosely based on django.contrib.comments.
+Template tags for the notes app, loosely based on django.contrib.comments.
 
 """
 from django import template
-from django.contrib.contenttypes.models import ContentType
 
-from classytags.arguments import Argument
-from classytags.core import Options
-from classytags.helpers import AsTag, InclusionTag
-
-from photos.models import Photo
+from generic.tags import GetGenericRelationList, RenderGenericRelationList
+from ..models import Photo
 
 register = template.Library()
 
 
-class PhotosMixin(object):
-
-    def get_photos(self, target):
-        return Photo.objects.filter(
-            content_type=ContentType.objects.get_for_model(target),
-            object_id=target.pk,
-        )
-
-
-class RenderPhotoList(PhotosMixin, InclusionTag):
-    name = 'render_photo_list'
-    options = Options(
-        'for',
-        Argument('target', required=True, resolve=True)
-    )
-    template = 'photos/photo_list.html'
-
-    def get_context(self, context, target):
-        return {
-            'photos': self.get_photos(target),
-        }
+class RenderPhotoList(RenderGenericRelationList):
+    model = Photo
 
 register.tag(RenderPhotoList)
 
-
-class GetPhotoList(PhotosMixin, AsTag):
-    name = 'get_photo_list'
-    options = Options(
-        'for',
-        Argument('target', required=True, resolve=True),
-        'as',
-        Argument('varname', required=True, resolve=False),
-    )
-
-    def get_value(self, context, target):
-        return self.get_photos(target)
+class GetPhotoList(GetGenericRelationList):
+    model = Photo
 
 register.tag(GetPhotoList)

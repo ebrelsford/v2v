bdd612ae835ebbceb602e33a57ab341cd78871af
Eric Brelsford <ebrelsford@gmail.com>
2013-03-22 16:13:05 -0400

    management command for synchronizing data

---
+++ vacant_to_vibrant/sync/management/commands/syncdata.py
@@ -0,0 +1,26 @@
+import sys
+import traceback
+
+from django.core.management.base import BaseCommand
+
+from ...models import DataSource
+
+
+class Command(BaseCommand):
+    help = 'Synchronize data sources'
+
+    def handle(self, *args, **options):
+        self.stdout.write('sync: Running syncdata')
+
+        for data_source_cls in DataSource.__subclasses__():
+            print 'sync: Synchronizing %s' % data_source_cls.__name__
+            for data_source in data_source_cls.objects.all():
+                try:
+                    data_source.synchronize()
+                except Exception:
+                    print 'There was an exception while synchronizing %s' % (
+                        data_source,)
+                    traceback.print_exc(file=sys.stdout)
+                    continue
+
+        self.stdout.write('sync: Done running syncdata')

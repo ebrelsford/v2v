aa73f59b7184a21fabce1508ab5253fda5b9495f
Eric Brelsford <ebrelsford@gmail.com>
2013-03-22 15:43:41 -0400

    show whether AvailableProperty has lots

--- vacant_to_vibrant/phillydata/availableproperties/admin.py
+++ vacant_to_vibrant/phillydata/availableproperties/admin.py
@@ -8,8 +8,18 @@ from .models import AvailableProperty
 
 class AvailablePropertyAdmin(OSMGeoAdmin, CompareVersionAdmin):
     date_hierarchy = 'last_seen'
-    list_display = ('address', 'price_str', 'area', 'status', 'last_seen',)
+    list_display = ('address', 'price_str', 'area', 'status', 'last_seen',
+                    'has_lot',)
     list_filter = ('status', 'agency',)
     search_fields = ('address', 'description',)
+
+    def has_lot(self, obj):
+        count = obj.lot_set.all().count()
+        if count == 0:
+            return 'no'
+        if count == 1:
+            return 'yes'
+        if count > 1:
+            return 'many'
 
 admin.site.register(AvailableProperty, AvailablePropertyAdmin)

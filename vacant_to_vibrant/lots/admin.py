f0f27845a57d42012b95dfbd2c6642a804ffc35b
Eric Brelsford <ebrelsford@gmail.com>
2013-03-26 00:52:49 -0400

    better lot admin

--- vacant_to_vibrant/lots/admin.py
+++ vacant_to_vibrant/lots/admin.py
@@ -1,12 +1,51 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Lot
 
 
 class LotAdmin(OSMGeoAdmin, admin.ModelAdmin):
-    exclude = ('centroid',)
+    exclude = ('available_property', 'parcel',)
     list_display = ('address_line1', 'city', 'owner', 'billing_account',)
-    readonly_fields = ('land_use_area', 'parcel', 'violations',)
+    readonly_fields = ('added', 'available_property_link', 'billing_account',
+                       'land_use_area', 'owner', 'parcel_link', 'tax_account',
+                       'violations', 'water_parcel',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'address_line1', 'address_line2', 'city',
+                       'state_province', 'postal_code',),
+        }),
+        ('Other data', {
+            'classes': ('collapse',),
+            'fields': ('owner', 'billing_account', 'tax_account',
+                       'parcel_link', 'land_use_area', 'violations',
+                       'available_property_link', 'water_parcel', 'added',),
+        }),
+        ('Geography', {
+            'classes': ('collapse',),
+            'fields': ('centroid', 'polygon',),
+        }),
+    )
+
+    def available_property_link(self, obj):
+        change_url = reverse(
+            'admin:availableproperties_availableproperty_change',
+            args=(obj.available_property.pk,)
+        )
+        return mark_safe('<a href="%s">%s</a>' % (change_url,
+                                                  str(obj.available_property)))
+    available_property_link.short_description = 'public available property record'
+
+    def parcel_link(self, obj):
+        change_url = reverse(
+            'admin:parcels_parcel_change',
+            args=(obj.parcel.pk,)
+        )
+        return mark_safe('<a href="%s">%s</a>' % (change_url, str(obj.parcel)))
+    parcel_link.short_description = 'parcel'
+
 
 admin.site.register(Lot, LotAdmin)

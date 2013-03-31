c0e196222aab71cd331517707a4a6e1b39ae49ab
Eric Brelsford <ebrelsford@gmail.com>
2013-03-31 19:38:39 -0400

    bring tests in line with other phillydata tests

--- vacant_to_vibrant/phillydata/waterdept/tests.py
+++ vacant_to_vibrant/phillydata/waterdept/tests.py
@@ -28,127 +28,118 @@ class ApiTest(TestCase):
 class AdapterTest(TestCase):
     """Tests the adapter."""
 
-    example_response = """
-{
-    "MinCharge": false,
-    "Parcel": {
-        "Owner1": "OWNER NAME",
-        "ImpervCredit": 0,
-        "BRTAcct": "123456789",
-        "Owner2": null,
-        "FinalImperv": 500,
-        "FinalGross": 1000,
-        "GrossArea": 968,
-        "BldgType": "NonRes",
-        "NPDESCredit": 0,
-        "ParcelID": 123456,
-        "TenCode": "1234567890",
-        "Shape": "POLYGON ((2703814.74156681 254477.754747897, 2703813.02798755 254463.902413398, 2703743.47825789 254472.777067557, 2703745.30371356 254486.647118554, 2703814.74156681 254477.754747897))",
-        "ImpervArea": 242,
-        "Address": "1234 ABC ST",
-        "GrossCredit": 0,
-        "BldgDesc": "VAC LAND RES 003c ACRE",
-        "BldgCode": "SR"
-    },
-    "Area": 978.986368503422,
-    "Length": 168.066160089772,
-    "Accounts": [
-        {
-            "AccountStatusAbbrev": "D",
-            "WaterAccount": "0987654321234",
-            "HouseNum": "1234",
-            "ControlDay": "023",
-            "ServiceTypeLabel": "R - (no description)",
-            "Fire": "N",
-            "InstID": 123456,
-            "StormwaterStatus": "Not Billed",
-            "MeterSizeAbbrev": null,
+    record = {
+        "MinCharge": False,
+        "Parcel": {
+            "Owner1": "OWNER NAME",
+            "ImpervCredit": 0,
+            "BRTAcct": "123456789",
+            "Owner2": None,
+            "FinalImperv": 500,
+            "FinalGross": 1000,
+            "GrossArea": 968,
+            "BldgType": "NonRes",
+            "NPDESCredit": 0,
             "ParcelID": 123456,
-            "ServiceType": "3R",
-            "AccountNumber": "023-09876-54321-234",
             "TenCode": "1234567890",
-            "AcctStatus": "Discontinued",
-            "StreetDir": null,
-            "MeterSize": "(no description)",
-            "NonBillCode": "NB5",
-            "CustomerID": 654321,
-            "StreetName": "ABC ST",
-            "CustomerName": null
+            "Shape": "POLYGON ((2703814.74156681 254477.754747897, 2703813.02798755 254463.902413398, 2703743.47825789 254472.777067557, 2703745.30371356 254486.647118554, 2703814.74156681 254477.754747897))",
+            "ImpervArea": 242,
+            "Address": "1234 ABC ST",
+            "GrossCredit": 0,
+            "BldgDesc": "VAC LAND RES 003c ACRE",
+            "BldgCode": "SR"
         },
-        {
-            "AccountStatusAbbrev": "C",
-            "WaterAccount": "0987654321235",
-            "HouseNum": "1234",
-            "ControlDay": "023",
-            "ServiceTypeLabel": "3 - Stormwater Only",
-            "Fire": "N",
-            "InstID": 123456,
-            "StormwaterStatus": "Billed",
-            "MeterSizeAbbrev": "A",
-            "ParcelID": 123456,
-            "ServiceType": "43A",
-            "AccountNumber": "023-09876-54321-234",
-            "TenCode": "1234567890",
-            "AcctStatus": "Current",
-            "StreetDir": null,
-            "MeterSize": "No Meter",
-            "NonBillCode": null,
-            "CustomerID": 123456,
-            "StreetName": "ABC ST",
-            "CustomerName": "CUSTOMERNAME"
-        }
-    ],
-    "Y": 254475.274765976,
-    "X": 2703779.12088409,
-    "BufferedEnvelope": {
-        "XMin": 2703725.66243066,
-        "YMin": 254458.216237109,
-        "YMax": 254492.333294843,
-        "XMax": 2703832.55739404
-    },
-    "Match": {
-        "Type": "XY",
-        "Value": [
-            2703782.91588,
-            254472.639808
-        ]
-    },
-    "ImprvCount": 0
-}"""
+        "Area": 978.986368503422,
+        "Length": 168.066160089772,
+        "Accounts": [
+            {
+                "AccountStatusAbbrev": "D",
+                "WaterAccount": "0987654321234",
+                "HouseNum": "1234",
+                "ControlDay": "023",
+                "ServiceTypeLabel": "R - (no description)",
+                "Fire": "N",
+                "InstID": 123456,
+                "StormwaterStatus": "Not Billed",
+                "MeterSizeAbbrev": None,
+                "ParcelID": 123456,
+                "ServiceType": "3R",
+                "AccountNumber": "023-09876-54321-234",
+                "TenCode": "1234567890",
+                "AcctStatus": "Discontinued",
+                "StreetDir": None,
+                "MeterSize": "(no description)",
+                "NonBillCode": "NB5",
+                "CustomerID": 654321,
+                "StreetName": "ABC ST",
+                "CustomerName": None
+            },
+            {
+                "AccountStatusAbbrev": "C",
+                "WaterAccount": "0987654321235",
+                "HouseNum": "1234",
+                "ControlDay": "023",
+                "ServiceTypeLabel": "3 - Stormwater Only",
+                "Fire": "N",
+                "InstID": 123456,
+                "StormwaterStatus": "Billed",
+                "MeterSizeAbbrev": "A",
+                "ParcelID": 123456,
+                "ServiceType": "43A",
+                "AccountNumber": "023-09876-54321-234",
+                "TenCode": "1234567890",
+                "AcctStatus": "Current",
+                "StreetDir": None,
+                "MeterSize": "No Meter",
+                "NonBillCode": None,
+                "CustomerID": 123456,
+                "StreetName": "ABC ST",
+                "CustomerName": "CUSTOMERNAME"
+            }
+        ],
+        "Y": 254475.274765976,
+        "X": 2703779.12088409,
+        "BufferedEnvelope": {
+            "XMin": 2703725.66243066,
+            "YMin": 254458.216237109,
+            "YMax": 254492.333294843,
+            "XMax": 2703832.55739404
+        },
+        "Match": {
+            "Type": "XY",
+            "Value": [
+                2703782.91588,
+                254472.639808
+            ]
+        },
+        "ImprvCount": 0
+    }
 
     def test_parcel_kwargs(self):
-        data = json.loads(self.example_response)
-        kwargs = _water_parcel_kwargs(data)
+        kwargs = _water_parcel_kwargs(self.record)
         self.assertIsNotNone(kwargs)
         self.assertEqual(123456, kwargs['parcel_id'])
 
     def test_parcel_defaults(self):
-        data = json.loads(self.example_response)
-        defaults = _water_parcel_defaults(data)
+        defaults = _water_parcel_defaults(self.record)
         self.assertIsNotNone(defaults)
 
     def test_account_kwargs(self):
-        data = json.loads(self.example_response)
-
-        for account in data['Accounts']:
+        for account in self.record['Accounts']:
             kwargs = _water_account_kwargs(account)
             self.assertIsNotNone(kwargs)
 
     def test_account_defaults(self):
-        data = json.loads(self.example_response)
-
-        for account in data['Accounts']:
+        for account in self.record['Accounts']:
             defaults = _water_account_defaults(account)
             self.assertIsNotNone(defaults)
 
     def test_get_or_create_water_parcel(self):
-        data = json.loads(self.example_response)
-        water_parcel = _get_or_create_water_parcel(data)
+        water_parcel = _get_or_create_water_parcel(self.record)
         self.assertIsNotNone(water_parcel)
 
     def test_get_or_create_water_accounts(self):
-        data = json.loads(self.example_response)
-        water_parcel = _get_or_create_water_parcel(data)
-        water_accounts = _get_or_create_water_accounts(data, water_parcel)
+        water_parcel = _get_or_create_water_parcel(self.record)
+        water_accounts = _get_or_create_water_accounts(self.record, water_parcel)
         self.assertIsNotNone(water_accounts)
         self.assertNotEqual(0, len(water_accounts))

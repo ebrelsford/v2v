5297498bc6de4d451636cc3d86bbdb2b054ce1fc
Eric Brelsford <ebrelsford@gmail.com>
2013-04-04 16:29:47 -0400

    DRY content-forms

--- vacant_to_vibrant/photos/forms.py
+++ vacant_to_vibrant/photos/forms.py
@@ -1,39 +1,17 @@
-from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
-from django.forms import HiddenInput, IntegerField, ModelChoiceField
+from django import forms
+from django.utils.translation import ugettext_lazy as _
 
-from vacant_to_vibrant.forms import CaptchaForm
+from content.forms import ContentForm
 from .models import Photo
 
 
-class PhotoForm(CaptchaForm):
-    content_type = ModelChoiceField(
-        label='content type',
-        queryset=ContentType.objects.all(),
-        widget=HiddenInput()
+class PhotoForm(ContentForm):
+
+    original_image = forms.ImageField(
+        label=_('Photo file'),
     )
-
-    object_id = IntegerField(
-        label='object id',
-        widget=HiddenInput()
-    )
-
-    added_by = ModelChoiceField(
-        label='added_by',
-        queryset=User.objects.all(),
-        required=False,
-        widget=HiddenInput()
-    )
-
-    def __init__(self, *args, **kwargs):
-        # add initial value for added_by based on the user kwarg
-        kwargs['initial'] = kwargs.get('initial', {})
-        user = kwargs.get('user', None)
-        if not user or user.is_anonymous(): user = None
-        kwargs['initial']['added_by'] = user
-
-        super(PhotoForm, self).__init__(*args, **kwargs)
 
     class Meta:
-        exclude = ('added',)
+        fields = ('added_by_name', 'original_image', 'name', 'description',
+                  'content_type', 'object_id',)
         model = Photo

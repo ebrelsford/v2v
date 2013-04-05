832a02e133b46fb55a59c03731f36b04656c1495
Eric Brelsford <ebrelsford@gmail.com>
2013-04-04 20:53:00 -0400

    always disabling forms

---
+++ vacant_to_vibrant/static/js/main.js
@@ -0,0 +1,16 @@
+//
+// main.js
+//
+// Scripts that should run on every page.
+//
+
+$(document).ready(function() {
+
+    /*
+     * Disable submit buttons on forms once they have been submitted once.
+     */
+    $('form').submit(function() {
+        $(this).find('input[type="submit"]').attr('disabled', 'disabled');
+    });
+
+});

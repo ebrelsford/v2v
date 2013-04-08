385f73519c04740fac0a3231ef61e8390d64ffda
Eric Brelsford <ebrelsford@gmail.com>
2013-04-08 11:16:41 -0400

    better data source admin

--- vacant_to_vibrant/phillydata/admin.py
+++ vacant_to_vibrant/phillydata/admin.py
@@ -4,7 +4,24 @@ from .models import PhillyDataSource
 
 
 class PhillyDataSourceAdmin(admin.ModelAdmin):
+    actions = ('make_healthy',)
     list_display = ('name', 'healthy', 'synchronize_in_progress', 'ordering',
                     'last_synchronized', 'next_synchronize',)
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'ordering',),
+        }),
+        ('Status', {
+            'fields': (('synchronize_in_progress', 'healthy', 'enabled',),),
+        }),
+        ('Synchronization', {
+            'fields': (('last_synchronized', 'next_synchronize',
+                        'synchronize_frequency',),),
+        }),
+    )
+
+    def make_healthy(self, request, queryset):
+        queryset.update(healthy=True)
+    make_healthy.short_description = "Mark selected sources as healthy"
 
 admin.site.register(PhillyDataSource, PhillyDataSourceAdmin)

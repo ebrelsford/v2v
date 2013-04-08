b3d97bde22f535b7d156a86dbe64ba045b654c41
Eric Brelsford <ebrelsford@gmail.com>
2013-04-08 11:36:24 -0400

    enable / disable

--- vacant_to_vibrant/phillydata/admin.py
+++ vacant_to_vibrant/phillydata/admin.py
@@ -4,9 +4,9 @@ from .models import PhillyDataSource
 
 
 class PhillyDataSourceAdmin(admin.ModelAdmin):
-    actions = ('make_healthy',)
-    list_display = ('name', 'healthy', 'synchronize_in_progress', 'ordering',
-                    'last_synchronized', 'next_synchronize',)
+    actions = ('make_healthy', 'enable', 'disable',)
+    list_display = ('name', 'synchronize_in_progress', 'healthy', 'enabled',
+                    'ordering', 'last_synchronized', 'next_synchronize',)
     fieldsets = (
         (None, {
             'fields': ('name', 'description', 'ordering',),
@@ -26,5 +26,13 @@ class PhillyDataSourceAdmin(admin.ModelAdmin):
     def make_healthy(self, request, queryset):
         queryset.update(healthy=True)
     make_healthy.short_description = "Mark selected sources as healthy"
+
+    def enable(self, request, queryset):
+        queryset.update(enabled=True)
+    enable.short_description = "Enable sources"
+
+    def disable(self, request, queryset):
+        queryset.update(enabled=False)
+    disable.short_description = "Disable sources"
 
 admin.site.register(PhillyDataSource, PhillyDataSourceAdmin)

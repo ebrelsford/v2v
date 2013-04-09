90556aeb2ddad2a81832e9094b69f0021e38ea33
Eric Brelsford <ebrelsford@gmail.com>
2013-04-09 15:16:18 -0400

    just city council districts, please

--- vacant_to_vibrant/phillydata/citycouncil/admin.py
+++ vacant_to_vibrant/phillydata/citycouncil/admin.py
@@ -1,9 +1,24 @@
+from django import forms
 from django.contrib import admin
+
+from inplace.boundaries.models import Boundary
 
 from .models import CityCouncilMember
 
 
+class CityCouncilMemberForm(forms.ModelForm):
+    district = forms.ModelChoiceField(
+        queryset=Boundary.objects.filter(
+            layer__name='City Council Districts',
+        ),
+    )
+
+    class Meta:
+        model = CityCouncilMember
+
+
 class CityCouncilMemberAdmin(admin.ModelAdmin):
+    form = CityCouncilMemberForm
     list_display = ('name', 'district',)
     search_fields = ('name',)
 

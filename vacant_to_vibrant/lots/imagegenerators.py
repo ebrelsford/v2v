4bc385d63ff2370c694c53b31fa504c41d288af5
Eric Brelsford <ebrelsford@gmail.com>
2013-04-24 17:13:27 -0400

    more reasonable photo thumbnails

---
+++ vacant_to_vibrant/lots/imagegenerators.py
@@ -0,0 +1,9 @@
+from imagekit import ImageSpec, register
+from imagekit.processors import SmartResize
+
+class DetailPageThumbnail(ImageSpec):
+    processors = [SmartResize(100, 100)]
+    format = 'PNG'
+    options = {'quality': 80}
+
+register.generator('lots:detail_page_thumbnail', DetailPageThumbnail)

6dd06b03d637c54ee57a0349f784404ae905dc3c
Eric Brelsford <ebrelsford@gmail.com>
2013-04-24 16:46:51 -0400

    organize tags use generic tags

--- vacant_to_vibrant/organize/templatetags/organize_tags.py
+++ vacant_to_vibrant/organize/templatetags/organize_tags.py
@@ -3,53 +3,20 @@ Template tags for the organize app, loosely based on django.contrib.comments.
 
 """
 from django import template
-from django.contrib.contenttypes.models import ContentType
 
-from classytags.arguments import Argument
-from classytags.core import Options
-from classytags.helpers import AsTag, InclusionTag
-
-from organize.models import Organizer
+from generic.tags import GetGenericRelationList, RenderGenericRelationList
+from ..models import Organizer
 
 register = template.Library()
 
 
-class OrganizersMixin(object):
-
-    def get_organizers(self, target):
-        return Organizer.objects.filter(
-            content_type=ContentType.objects.get_for_model(target),
-            object_id=target.pk,
-        )
-
-
-class RenderOrganizerList(OrganizersMixin, InclusionTag):
-    name = 'render_organizer_list'
-    options = Options(
-        'for',
-        Argument('target', required=True, resolve=True)
-    )
-    template = 'organize/organizer_list.html'
-
-    def get_context(self, context, target):
-        context.update({
-            'organizers': self.get_organizers(target),
-        })
-        return context
+class RenderOrganizerList(RenderGenericRelationList):
+    model = Organizer
 
 register.tag(RenderOrganizerList)
 
 
-class GetOrganizerList(OrganizersMixin, AsTag):
-    name = 'get_organizer_list'
-    options = Options(
-        'for',
-        Argument('target', required=True, resolve=True),
-        'as',
-        Argument('varname', required=True, resolve=False),
-    )
-
-    def get_value(self, context, target):
-        return self.get_organizers(target)
+class GetOrganizerList(GetGenericRelationList):
+    model = Organizer
 
 register.tag(GetOrganizerList)

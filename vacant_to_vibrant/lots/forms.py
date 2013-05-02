faa7e573e802b829699ea842587315eb01c7f2d2
Eric Brelsford <ebrelsford@gmail.com>
2013-05-02 16:29:56 -0400

    rearrange filters

--- vacant_to_vibrant/lots/forms.py
+++ vacant_to_vibrant/lots/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
+from chosen.forms import ChosenSelectMultiple
 from inplace.boundaries.models import Boundary, Layer
 
 from phillydata.zoning.models import ZoningType
@@ -66,8 +67,9 @@ class FiltersForm(forms.Form):
     )
 
     zoning_district__zoning_type__in = forms.ModelMultipleChoiceField(
+        label=_('Zoning type'),
         queryset=ZoningType.objects.all(),
-        widget=forms.CheckboxSelectMultiple(),
+        widget=ChosenSelectMultiple(),
     )
 
     known_use__name__in = forms.MultipleChoiceField(
@@ -92,7 +94,7 @@ class FiltersForm(forms.Form):
             choices=[(b.label, b.label) for b in boundaries],
             initial=(),
             label=_(layer.name),
-            widget=forms.CheckboxSelectMultiple(),
+            widget=ChosenSelectMultiple(),
         )
 
     def _get_uses(self):
@@ -100,3 +102,22 @@ class FiltersForm(forms.Form):
         for use in Use.objects.all().order_by('name'):
             uses += [(use.name, use.name),]
         return uses
+
+    def admin_filters(self):
+        for field in ('participant_types', 'has_available_property',
+                      'has_billing_account', 'has_tax_account', 'has_parcel',
+                      'has_land_use_area'):
+            yield self[field]
+
+    def other_filters(self):
+        for field in ('boundary_zipcodes', 'boundary_city_council_districts',
+                      'zoning_district__zoning_type__in'):
+            yield self[field]
+
+    def owners_filters(self):
+        for field in ('owner__name__icontains', 'owner__owner_type__in',):
+            yield self[field]
+
+    def private_filters(self):
+        for field in ('has_violations', 'violations_count',):
+            yield self[field]

7b044eaef59295ee523226bfabb2001339babb8f
Eric Brelsford <ebrelsford@gmail.com>
2013-05-06 17:25:56 -0400

    update map viewport when linking to a map view

--- vacant_to_vibrant/static/js/leaflet.geojsonbounds.js
+++ vacant_to_vibrant/static/js/leaflet.geojsonbounds.js
@@ -1,6 +1,17 @@
 
 L.extend(L.LatLngBounds.prototype, {
 
+    fromGeoJson: function(geoJson) {
+        var ne = geoJson.coordinates[0][2],
+            sw = geoJson.coordinates[0][0];
+
+        // Construct LatLngBounds, swapping x,y to get lat,lng
+        return L.latLngBounds([
+            [ne[1], ne[0]],
+            [sw[1], sw[0]]
+        ]);
+    },
+
     toGeoJson: function() {
         return {
             'type': 'Polygon',
@@ -15,3 +26,13 @@ L.extend(L.LatLngBounds.prototype, {
     },
 
 });
+
+
+/*
+ * Shortcut for creating LatLngBounds out of a GeoJSON string representing the
+ * bounding box to be converted to LatLngBounds.
+ */
+L.geoJsonLatLngBounds = function(geoJsonString) {
+    var geoJson = JSON.parse(geoJsonString);
+    return L.LatLngBounds.prototype.fromGeoJson(geoJson);
+};

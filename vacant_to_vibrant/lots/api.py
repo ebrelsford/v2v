378ac9164e9e1b521f625f6400c24520b0a36467
Eric Brelsford <ebrelsford@gmail.com>
2013-05-08 13:55:12 -0400

    documentation

--- vacant_to_vibrant/lots/api.py
+++ vacant_to_vibrant/lots/api.py
@@ -42,20 +42,26 @@ class LotResource(ModelResource):
 
     def build_filters(self, filters={}):
         orm_filters = super(LotResource, self).build_filters(filters=filters)
+
+        # Remove empty bbox filter
         if 'centroid__within' in orm_filters and orm_filters['centroid__within'] == '':
             del orm_filters['centroid__within']
 
+        # Remove empty owner name filter
         if 'owner__name__icontains' in orm_filters and orm_filters['owner__name__icontains'] == '':
             del orm_filters['owner__name__icontains']
 
+        # Add participant types
         orm_filters['participant_types'] = filters.getlist('participant_types', [])
 
+        # Add impervious area filters
         try:
             impervious_area = int(filters['water_parcel__impervious_area__lt'])
             orm_filters['water_parcel__impervious_area__lt'] = impervious_area
         except Exception:
             pass
 
+        # Add boundary filters
         for f in filters:
             if not f.startswith('boundary_'): continue
             orm_filters[f] = filters.getlist(f)
@@ -66,11 +72,14 @@ class LotResource(ModelResource):
         form.is_valid()
         cleaned_data = form.cleaned_data
 
+        # Add violations count filter
         orm_filters['violations_count'] = cleaned_data.get('violations_count', 0)
 
+        # Add parents only filter
         if cleaned_data.get('parents_only', False):
             orm_filters['group__isnull'] = True
 
+        # Add has_* filters (checking for null on foreign key fields)
         for f in ('available_property', 'billing_account', 'tax_account',
                   'parcel', 'land_use_area', 'licenses', 'violations',):
             filter_name = 'has_%s' % f
@@ -85,7 +94,9 @@ class LotResource(ModelResource):
 
     def apply_filters(self, request, applicable_filters):
 
+        #
         # Pop custom filters
+        #
         cleaned_filters = applicable_filters.copy()
 
         # Pop boundary filters for once we have a queryset
@@ -105,12 +116,14 @@ class LotResource(ModelResource):
         # Pop participant_types
         participant_types = cleaned_filters.pop('participant_types', [])
 
-
+        #
         # Get queryset using the orm filters
+        #
         qs = super(LotResource, self).apply_filters(request, cleaned_filters)
 
-
+        #
         # Apply custom filters
+        #
 
         # Apply boundary filters
         boundary_filters = None

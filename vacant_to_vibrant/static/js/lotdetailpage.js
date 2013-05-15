2c1717192eadc386fd9ebe2877ca5a1f6b2ff37a
Eric Brelsford <ebrelsford@gmail.com>
2013-05-15 00:50:35 -0400

    lot detail polygon color

--- vacant_to_vibrant/static/js/lotdetailpage.js
+++ vacant_to_vibrant/static/js/lotdetailpage.js
@@ -1,4 +1,5 @@
 define(['jquery', 'leaflet', 'jquery.streetview'], function($, L) {
+
     $(document).ready(function() {
         $streetviewContainer = $('#streetview-container');
         $streetviewContainer.streetview({
@@ -8,45 +9,31 @@ define(['jquery', 'leaflet', 'jquery.streetview'], function($, L) {
             $streetviewContainer.data('lon'),
             $streetviewContainer.data('lat')
         );
-    });
 
-    var map = new L.Map('map');
-
-    var key = $('#map').data('cloudmadekey'),
-        style = $('#map').data('cloudmadestyle');
-    var cloudmade = new L.TileLayer(
-        'http://{s}.tile.cloudmade.com/' + key + '/' + style +'/256/{z}/{x}/{y}.png', 
-        {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-            maxZoom: 18,
-        }
-    );
-
-    map.addLayer(cloudmade);
-
-    var feature_layer = new L.GeoJSON(null, {
-
-        pointToLayer: function(data, latlng) {
-            return new L.marker(latlng);
-        },
-
-        style: function(feature) {
-            return {
-                radius: 1,
-                fillColor: "#000000",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8,
+        var map = new L.Map('map');
+        var key = $('#map').data('cloudmadekey'),
+            style = $('#map').data('cloudmadestyle');
+        var cloudmade = new L.TileLayer(
+            'http://{s}.tile.cloudmade.com/' + key + '/' + style +'/256/{z}/{x}/{y}.png', 
+            {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+                maxZoom: 18,
             }
-        },
+        );
+        map.addLayer(cloudmade);
 
-    });
+        $.get($('#map').data('url'), function(data) {
+            var style = {
+                fillColor: 'hsl(140, 42%, 40%)',
+                fillOpacity: .7,
+                color: 'white',
+                opacity: .8,
+                weight: 2,
+            };
+            var feature_layer = new L.GeoJSON(data, { style: style, }).addTo(map);
+            map.fitBounds(feature_layer.getBounds());
+        });
 
-    $.get($('#map').data('url'), function(collection) {
-        feature_layer.addData(collection);
-        map.addLayer(feature_layer);
-        map.fitBounds(feature_layer.getBounds());
     });
 
 });

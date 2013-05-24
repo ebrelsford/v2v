934f2903d7a8fff42110f23a6066d824fa957314
Eric Brelsford <ebrelsford@gmail.com>
2013-05-24 15:27:02 -0400

    addorganizerpage module

---
+++ vacant_to_vibrant/static/js/addorganizerpage.js
@@ -0,0 +1,26 @@
+/*
+ * Module for the add organizer page.
+ */
+define(['jquery',], function($) {
+    function toggle_cbo_fields(show) {
+        var $cbo_fields = $(':input[name="facebook_page"],:input[name="url"]').parents('tr');
+        if (show) {
+            $cbo_fields.show();
+        }
+        else {
+            $cbo_fields.hide();
+        }
+    }
+
+    function is_cbo() {
+        return ($(':input[name="type"] :selected').text() === 'community based organization');
+    }
+
+    $(document).ready(function() {
+        toggle_cbo_fields(is_cbo());
+
+        $(':input[name="type"]').change(function() {
+            toggle_cbo_fields(is_cbo());
+        });
+    });
+});
--- vacant_to_vibrant/static/js/main.js
+++ vacant_to_vibrant/static/js/main.js
@@ -33,29 +33,7 @@ define(['jquery'], function($) {
     }
 
     if ($('.add-organizer-page').length > 0) {
-        function toggle_cbo_fields(show) {
-            var $cbo_fields = $(':input[name="facebook_page"],:input[name="url"]').parents('tr');
-            if (show) {
-                $cbo_fields.show();
-            }
-            else {
-                $cbo_fields.hide();
-            }
-        }
-
-        function is_cbo() {
-            return ($(':input[name="type"] :selected').text() 
-                === 'community based organization');
-        }
-
-        $(document).ready(function() {
-            toggle_cbo_fields(is_cbo());
-
-            $(':input[name="type"]').change(function() {
-                toggle_cbo_fields(is_cbo());
-            });
-        });
-        
+        require(['addorganizerpage']);
     }
 
     if ($('.home-map-page').length > 0) {

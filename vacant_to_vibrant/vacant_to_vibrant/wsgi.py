49eb8c6c89cc0f79ee773bf973fe675e7d87f3f7
Eric Brelsford <ebrelsford@gmail.com>
2013-05-24 13:52:22 -0400

    wsgi that should work anywhere with virtualenv

--- vacant_to_vibrant/vacant_to_vibrant/wsgi.py
+++ vacant_to_vibrant/vacant_to_vibrant/wsgi.py
@@ -14,16 +14,8 @@ framework.
 
 """
 import os
-import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vacant_to_vibrant.settings")
-
-activate_this = os.path.expanduser("~/.virtualenvs/v2v/bin/activate_this.py")
-execfile(activate_this, dict(__file__=activate_this))
-
-project = os.path.expanduser('~/webapps/django_gu/v2v/vacant_to_vibrant')
-workspace = os.path.dirname(project)
-sys.path.append(workspace)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

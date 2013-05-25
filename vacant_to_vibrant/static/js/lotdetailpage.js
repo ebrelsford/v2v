d67ef58399dfc1660f9c99dc9578cc18e38962a9
Eric Brelsford <ebrelsford@gmail.com>
2013-05-25 16:36:00 -0400

    jshint

--- vacant_to_vibrant/static/js/lotdetailpage.js
+++ vacant_to_vibrant/static/js/lotdetailpage.js
@@ -1,9 +1,9 @@
 /*
  * Module for the lot detail page.
  */
-define(['jquery', 'fancybox',], function($) {
+define(['jquery', 'fancybox',], function ($) {
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('.fancybox').fancybox();
     });
 

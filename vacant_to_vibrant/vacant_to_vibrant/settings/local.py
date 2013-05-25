6681a5284fd41ec959c6c3584cbd801187fda8cd
Eric Brelsford <ebrelsford@gmail.com>
2013-05-25 13:25:38 -0400

    ipdb

--- vacant_to_vibrant/vacant_to_vibrant/settings/local.py
+++ vacant_to_vibrant/vacant_to_vibrant/settings/local.py
@@ -41,10 +41,12 @@ INTERNAL_IPS = ('127.0.0.1',)
 
 MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django_pdb.middleware.PdbMiddleware',
 )
 
 INSTALLED_APPS += (
     'debug_toolbar',
+    'django_pdb',
 )
 
 DEBUG_TOOLBAR_PANELS = (

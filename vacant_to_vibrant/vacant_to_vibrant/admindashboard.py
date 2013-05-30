542b0a78cff08b96e2cbd53065bd42624700afe5
Eric Brelsford <ebrelsford@gmail.com>
2013-05-30 16:45:31 -0400

    django-admin-tools with custom dashboard

---
+++ vacant_to_vibrant/vacant_to_vibrant/admindashboard.py
@@ -0,0 +1,65 @@
+from django.utils.translation import ugettext_lazy as _
+from admin_tools.dashboard import modules, Dashboard
+
+
+class PhillyDashboard(Dashboard):
+    columns = 3
+
+    def __init__(self, **kwargs):
+
+        self.children = self.children or []
+
+        self.children.append(modules.ModelList(
+            title=_('Site Content'),
+            models=(
+                'elephantblog.*',
+                'feincms.module.page.*',
+                'pathways.*',
+            ),
+        ))
+
+        self.children.append(modules.ModelList(
+            title=_('Lots'),
+            models=(
+                'lots.*',
+            ),
+        ))
+
+        self.children.append(modules.ModelList(
+            title=_('Organize'),
+            models=(
+                'phillyorganize.*',
+            ),
+        ))
+
+        self.children.append(modules.AppList(
+            title=_('Applications'),
+            exclude=(
+                'django.contrib.*',
+                'elephantblog.*',
+                'feincms.module.page.*',
+                'phillydata.*',
+                'libapps.content.*',
+                'lots.*',
+            ),
+        ))
+
+        self.children.append(modules.ModelList(
+            title=_('Lot Content'),
+            models=('libapps.content.*',),
+        ))
+
+        self.children.append(modules.AppList(
+            title=_('Data'),
+            models=('phillydata.*',),
+        ))
+
+        self.children.append(modules.AppList(
+            title=_('Administration'),
+            models=('django.contrib.*',),
+        ))
+
+        self.children.append(modules.RecentActions(
+            title=_('Recent Actions'),
+            limit=5
+        ))

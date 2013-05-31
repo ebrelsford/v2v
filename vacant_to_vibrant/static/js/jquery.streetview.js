d85b6a9af30a6eb668e9f194b2c2b96bcff2cfbd
Eric Brelsford <ebrelsford@gmail.com>
2013-05-31 14:31:36 -0400

    try to point streetview at lots

--- vacant_to_vibrant/static/js/jquery.streetview.js
+++ vacant_to_vibrant/static/js/jquery.streetview.js
@@ -1,7 +1,7 @@
-define(['jquery', 'jquery.plugin',], function($) {
+define(['jquery', 'jquery.plugin',], function ($) {
     var StreetView = {
 
-        init: function(options, elem) {
+        init: function (options, elem) {
             this.options = $.extend({}, this.options, options);
 
             this.elem = elem;
@@ -17,7 +17,22 @@ define(['jquery', 'jquery.plugin',], function($) {
             errorSelector: '#streetview-error',
         },
 
-        load_streetview: function(lon, lat) {
+        get_heading: function (lon0, lat0, lon1, lat1) {
+            // Don't bother with great-circle calculations--should be close!
+            var r = Math.atan2(-(lon1 - lon0), (lat1 - lat0));
+            if (r < 0) {
+                r += 2 * Math.PI;
+            }
+            var d = r * (180 / Math.PI);
+
+            // Convert to google's heading: "True north is 0°, east is 90°,
+            // south is 180°, west is 270°."
+            if (d >= 45 && d < 135) { d += 180; }
+            else if (d >= 225 && d < 315) { d -= 180; }
+            return d;
+        },
+
+        load_streetview: function (lon, lat) {
             var instance = this;
             instance.$elem.hide();
             $(instance.options.errorSelector).hide();
@@ -25,11 +40,18 @@ define(['jquery', 'jquery.plugin',], function($) {
             if (!(lon && lat)) return;
             var latLng = new google.maps.LatLng(lat, lon);
 
-            instance.service.getPanoramaByLocation(latLng, 50, function(result, status) {
-                if (status == google.maps.StreetViewStatus.OK) {
+            instance.service.getPanoramaByLocation(latLng, 50, function (result, status) {
+                if (status === google.maps.StreetViewStatus.OK) {
                     instance.$elem.show();
+                    var lon0 = result.location.latLng.lng(),
+                        lat0 = result.location.latLng.lat();
+
                     new google.maps.StreetViewPanorama(instance.elem, {
                         pano: result.location.pano,
+                        pov: {
+                            heading: instance.get_heading(lon0, lat0, lon, lat),
+                            pitch: 0,
+                        },
                     });
                 }
                 else {

76d29283fe556871e59819ecc33cd60e089c7424
Eric Brelsford <ebrelsford@gmail.com>
2013-06-03 11:03:29 -0400

    jshint

--- vacant_to_vibrant/static/js/jquery.searchbar.js
+++ vacant_to_vibrant/static/js/jquery.searchbar.js
@@ -1,82 +1,91 @@
-define(['jquery', 'geocode', 'jquery.plugin',], function($, geocode) {
-    var SearchBar = {
+define(
+    [
+        'jquery',
+        'geocode',
 
-        init: function(options, elem) {
-            var instance = this;
-            instance.options = $.extend({}, instance.options, options);
-            instance.elem = elem;
-            instance.$elem = $(elem);
+        'jquery.plugin',
+        'lib/jquery.spin',
 
-            instance.$elem.keypress(function(e) {
-                if (e.keyCode == '13') {
+    ], function ($, geocode) {
+        var SearchBar = {
+
+            init: function (options, elem) {
+                var instance = this;
+                instance.options = $.extend({}, instance.options, options);
+                instance.elem = elem;
+                instance.$elem = $(elem);
+
+                instance.$elem.keypress(function (e) {
+                    if (e.keyCode === '13') {
+                        e.preventDefault();
+                        instance.searchByAddress();
+                        return false;
+                    }
+                });
+                instance.$elem.find('form').submit(function (e) {
                     e.preventDefault();
                     instance.searchByAddress();
                     return false;
-                }
-            });
-            instance.$elem.find('form').submit(function(e) {
-                e.preventDefault();
-                instance.searchByAddress();
-                return false;
-            });
-        },
+                });
+            },
 
 
-        options: {
-            bounds: null,
-            city: null,
-            state: null,
-            errorMessage: null,
-            loadingSelector: null,
-            warningSelector: null,
-        },
+            options: {
+                bounds: null,
+                city: null,
+                state: null,
+                errorMessage: null,
+                loadingSelector: null,
+                warningSelector: null,
+            },
 
-        addCityAndState: function(query) {
-            var city = this.options.city.toLowerCase();
-            if (!query.toLowerCase().indexOf(city) >= 0) {
-                query += ', ' + city;
-            }
-
-            var state = this.options.state.toLowerCase();
-            if (!query.toLowerCase().indexOf(state) >= 0) {
-                query += ', ' + state;
-            }
-            return query;
-        },
-
-        searchByAddress: function() {
-            var instance = this;
-            instance.$elem.find(instance.options.warningSelector).hide();
-            instance.$elem.find(instance.options.loadingSelector).show();
-
-            var query = instance.$elem.find('input[type="text"]').val();
-            query = instance.addCityAndState(query);
-
-            geocode(query, instance.options.bounds, instance.options.state, function(result, status) {
-                // Done searching
-                instance.$elem.find(instance.options.loadingSelector).hide();
-                instance.$elem.find('input[type="submit"]').removeAttr('disabled');
-
-                // Is result valid?
-                if (result === null) {
-                    instance.$elem.find(instance.options.warningSelector)
-                        .text(instance.options.errorMessage)
-                        .show();
-                    return;
+            addCityAndState: function (query) {
+                var city = this.options.city.toLowerCase();
+                if (query.toLowerCase().indexOf(city) <= 0) {
+                    query += ', ' + city;
                 }
 
-                // Let the world know!
-                var found_location = result.geometry.location;
-                instance.$elem.trigger('searchresultfound', [{
-                    longitude: found_location.lng(),
-                    latitude: found_location.lat(),
-                    query_address: query,
-                    found_address: result.formatted_address,
-                }]);
-            });
-        },
+                var state = this.options.state.toLowerCase();
+                if (query.toLowerCase().indexOf(state) <= 0) {
+                    query += ', ' + state;
+                }
+                return query;
+            },
 
-    };
+            searchByAddress: function () {
+                var instance = this;
+                instance.$elem.find(instance.options.warningSelector).hide();
+                instance.$elem.find(instance.options.loadingSelector).show();
 
-    $.plugin('searchbar', SearchBar);
-});
+                var query = instance.$elem.find('input[type="text"]').val();
+                query = instance.addCityAndState(query);
+
+                geocode(query, instance.options.bounds, instance.options.state, function (result, status) {
+                    // Done searching
+                    instance.$elem.find(instance.options.loadingSelector).hide();
+                    instance.$elem.find('input[type="submit"]').removeAttr('disabled');
+
+                    // Is result valid?
+                    if (result === null) {
+                        instance.$elem.find(instance.options.warningSelector)
+                            .text(instance.options.errorMessage)
+                            .show();
+                        return;
+                    }
+
+                    // Let the world know!
+                    var found_location = result.geometry.location;
+                    instance.$elem.trigger('searchresultfound', [{
+                        longitude: found_location.lng(),
+                        latitude: found_location.lat(),
+                        query_address: query,
+                        found_address: result.formatted_address,
+                    }]);
+                });
+            },
+
+        };
+
+        $.plugin('searchbar', SearchBar);
+    }
+);

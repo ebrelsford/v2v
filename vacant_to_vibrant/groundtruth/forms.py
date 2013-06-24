99bae2e0009b5a3391b7c42246f78d4b3fb350a7
Eric Brelsford <ebrelsford@gmail.com>
2013-06-24 19:23:17 -0400

    Groundtruth: require contact info

--- vacant_to_vibrant/groundtruth/forms.py
+++ vacant_to_vibrant/groundtruth/forms.py
@@ -1,9 +1,19 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from .models import GroundtruthRecord
 
 
 class GroundtruthRecordForm(forms.ModelForm):
+
+    def clean(self):
+        cleaned_data = super(GroundtruthRecordForm, self).clean()
+        contact_email = cleaned_data.get('contact_email', None)
+        contact_phone = cleaned_data.get('contact_phone', None)
+        if not (contact_email or contact_phone):
+            raise forms.ValidationError(_('Please enter an email address or '
+                                          'phone number'))
+        return cleaned_data
 
     class Meta:
         model = GroundtruthRecord

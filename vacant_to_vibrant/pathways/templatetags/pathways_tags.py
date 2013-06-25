ea78aa5d7a3480508d202144e5a4b6fd8860c823
Eric Brelsford <ebrelsford@gmail.com>
2013-06-25 17:08:55 -0400

    Pathways tags

--- vacant_to_vibrant/pathways/templatetags/pathways_tags.py
+++ vacant_to_vibrant/pathways/templatetags/pathways_tags.py
@@ -2,14 +2,36 @@ from django import template
 
 from classytags.arguments import Argument
 from classytags.core import Options
-from classytags.helpers import AsTag
+from classytags.helpers import AsTag, InclusionTag
 
 from ..models import Pathway
 
 register = template.Library()
 
 
-class GetPathways(AsTag):
+class PathwaysTag(object):
+
+    def get_objects(self, target):
+        return Pathway.objects.get_for_lot(target)
+
+
+class RenderPathwaySummaryList(PathwaysTag, InclusionTag):
+    options = Options(
+        'for',
+        Argument('target', required=True, resolve=True)
+    )
+    template = 'pathways/pathway_summary_list.html'
+
+    def get_context(self, context, target):
+        context.update({
+            'pathways': self.get_objects(target),
+        })
+        return context
+
+register.tag(RenderPathwaySummaryList)
+
+
+class GetPathways(PathwaysTag, AsTag):
     options = Options(
         'for',
         Argument('target', required=True, resolve=True),
@@ -18,6 +40,6 @@ class GetPathways(AsTag):
     )
 
     def get_value(self, context, target):
-        return Pathway.objects.get_for_lot(target)
+        return self.get_objects(target)
 
 register.tag(GetPathways)

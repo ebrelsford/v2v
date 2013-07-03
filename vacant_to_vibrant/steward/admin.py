f063b8e4fcdade5cdec9ecab6960a75c8c98a9cf
Eric Brelsford <ebrelsford@gmail.com>
2013-07-03 17:47:10 -0400

    stewarded_target for all steward admins

--- vacant_to_vibrant/steward/admin.py
+++ vacant_to_vibrant/steward/admin.py
@@ -6,12 +6,7 @@ from django_monitor.admin import MonitorAdmin
 from .models import StewardNotification, StewardProject
 
 
-class StewardNotificationAdmin(EnhancedModelAdminMixin, MonitorAdmin):
-    fields = ('stewarded_target', 'name', 'use', 'support_organization',
-              'land_tenure_status', 'include_on_map', 'phone', 'email', 'type',
-              'url', 'facebook_page',)
-    list_display = ('pk', 'name', 'stewarded_target',)
-    readonly_fields = ('content_type', 'object_id', 'stewarded_target',)
+class BaseStewardAdminMixin(EnhancedModelAdminMixin):
 
     def stewarded_target(self, obj):
         try:
@@ -24,16 +19,24 @@ class StewardNotificationAdmin(EnhancedModelAdminMixin, MonitorAdmin):
     stewarded_target.allow_tags = True
 
 
-class StewardProjectAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'address', 'organizer', 'use',
+class StewardNotificationAdmin(BaseStewardAdminMixin, MonitorAdmin):
+
+    fields = ('stewarded_target', 'name', 'use', 'support_organization',
+              'land_tenure_status', 'include_on_map', 'phone', 'email', 'type',
+              'url', 'facebook_page',)
+    list_display = ('pk', 'name', 'stewarded_target',)
+    readonly_fields = ('content_type', 'object_id', 'stewarded_target',)
+
+
+class StewardProjectAdmin(BaseStewardAdminMixin, admin.ModelAdmin):
+
+    fields = ('stewarded_target', 'name', 'use', 'support_organization',
+              'land_tenure_status', 'include_on_map', 'organizer',
+              'date_started', 'external_id',)
+    list_display = ('pk', 'name', 'stewarded_target', 'organizer', 'use',
                     'include_on_map',)
     list_filter = ('use', 'include_on_map',)
-
-    def address(self, obj):
-        try:
-            return obj.content_object.address_line1
-        except Exception:
-            return ''
+    readonly_fields = ('content_type', 'object_id', 'stewarded_target',)
 
 
 admin.site.register(StewardNotification, StewardNotificationAdmin)

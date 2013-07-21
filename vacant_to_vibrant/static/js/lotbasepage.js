da0a58ffa9a96e779d2a714a4d158d16e406474f
Eric Brelsford <ebrelsford@gmail.com>
2013-07-21 14:25:11 -0400

    jshint

--- vacant_to_vibrant/static/js/lotbasepage.js
+++ vacant_to_vibrant/static/js/lotbasepage.js
@@ -7,44 +7,45 @@ define(
         'leaflet',
         'jquery.streetview',
         'lib/bootstrap/bootstrap-tooltip'
-    ], function($, L) {
+    ], function ($, L) {
 
-    $(document).ready(function() {
-        $streetviewContainer = $('#streetview-container');
-        $streetviewContainer.streetview({
-            errorSelector: '#streetview-error',
+        $(document).ready(function () {
+            var $streetviewContainer = $('#streetview-container');
+            $streetviewContainer.streetview({
+                errorSelector: '#streetview-error',
+            });
+            $streetviewContainer.data('streetview').load_streetview(
+                $streetviewContainer.data('lon'),
+                $streetviewContainer.data('lat')
+            );
+
+            var map = new L.Map('map');
+            var key = $('#map').data('cloudmadekey'),
+                style = $('#map').data('cloudmadestyle');
+            var cloudmade = new L.TileLayer(
+                'http://{s}.tile.cloudmade.com/' + key + '/' + style + '/256/{z}/{x}/{y}.png',
+                {
+                    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+                    maxZoom: 18,
+                }
+            );
+            map.addLayer(cloudmade);
+
+            $.get($('#map').data('url'), function (data) {
+                var style = {
+                    fillColor: 'hsl(140, 42%, 40%)',
+                    fillOpacity: 0.7,
+                    color: 'white',
+                    opacity: 0.8,
+                    weight: 2,
+                };
+                var feature_layer = new L.GeoJSON(data, { style: style, }).addTo(map);
+                map.fitBounds(feature_layer.getBounds());
+            });
+
+            $('.build-community-button').tooltip();
+
         });
-        $streetviewContainer.data('streetview').load_streetview(
-            $streetviewContainer.data('lon'),
-            $streetviewContainer.data('lat')
-        );
 
-        var map = new L.Map('map');
-        var key = $('#map').data('cloudmadekey'),
-            style = $('#map').data('cloudmadestyle');
-        var cloudmade = new L.TileLayer(
-            'http://{s}.tile.cloudmade.com/' + key + '/' + style +'/256/{z}/{x}/{y}.png', 
-            {
-                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-                maxZoom: 18,
-            }
-        );
-        map.addLayer(cloudmade);
-
-        $.get($('#map').data('url'), function(data) {
-            var style = {
-                fillColor: 'hsl(140, 42%, 40%)',
-                fillOpacity: .7,
-                color: 'white',
-                opacity: .8,
-                weight: 2,
-            };
-            var feature_layer = new L.GeoJSON(data, { style: style, }).addTo(map);
-            map.fitBounds(feature_layer.getBounds());
-        });
-
-        $('.build-community-button').tooltip();
-
-    });
-
-});
+    }
+);

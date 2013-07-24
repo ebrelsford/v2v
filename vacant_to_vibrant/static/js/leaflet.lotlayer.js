6e65caf4c4b42181d869a81ae812933fbeb7077b
Eric Brelsford <ebrelsford@gmail.com>
2013-07-24 09:47:19 -0400

    jshint

--- vacant_to_vibrant/static/js/leaflet.lotlayer.js
+++ vacant_to_vibrant/static/js/leaflet.lotlayer.js
@@ -9,68 +9,69 @@ define(
         'lib/leaflet.lvector',
         'json2',
 
-    ], function(L, lvector, JSON) {
+    ], function (L, lvector, JSON) {
 
-    lvector.LotLayer = lvector.GeoJSONLayer.extend({
+        lvector.LotLayer = lvector.GeoJSONLayer.extend({
 
-        initialize: function(options) {
-            // Check for required parameters
-            for (var i = 0, len = this._requiredParams.length; i < len; i++) {
-                if (!options[this._requiredParams[i]]) {
-                    throw new Error("No \"" + this._requiredParams[i] +
-                        "\" parameter found.");
+            initialize: function (options) {
+                // Check for required parameters
+                for (var i = 0, len = this._requiredParams.length; i < len; i++) {
+                    if (!options[this._requiredParams[i]]) {
+                        throw new Error("No \"" + this._requiredParams[i] +
+                            "\" parameter found.");
+                    }
                 }
-            }
 
-            // Extend Layer
-            lvector.Layer.prototype.initialize.call(this, options);
+                // Extend Layer
+                lvector.Layer.prototype.initialize.call(this, options);
 
-            // Create an array to hold the features
-            this._vectors = [];
+                // Create an array to hold the features
+                this._vectors = [];
 
-            if (this.options.map) {
-                if (this.options.scaleRange && this.options.scaleRange instanceof Array && this.options.scaleRange.length === 2) {
-                    var z = this.options.map.getZoom();
-                    var sr = this.options.scaleRange;
-                    this.options.visibleAtScale = (z >= sr[0] && z <= sr[1]);
+                if (this.options.map) {
+                    if (this.options.scaleRange && this.options.scaleRange instanceof Array && this.options.scaleRange.length === 2) {
+                        var z = this.options.map.getZoom();
+                        var sr = this.options.scaleRange;
+                        this.options.visibleAtScale = (z >= sr[0] && z <= sr[1]);
+                    }
+                    this._show();
                 }
-                this._show();
-            }
-        },
+            },
 
-        options: {
-            filters: {},
-            url: null,
-        },
+            options: {
+                filters: {},
+                url: null,
+            },
 
-        _requiredParams: ["url",],
+            _requiredParams: ["url",],
 
-        _getFeatures: function() {        
-            // Only load when map at a visible zoom
-            if (!this.options.visibleAtScale) {
-                return;
-            }
+            _getFeatures: function () {        
+                // Only load when map at a visible zoom
+                if (!this.options.visibleAtScale) {
+                    return;
+                }
 
-            // Add bounds to filters
-            this.options.filters.centroid__within = JSON.stringify(
-                    this.getMap().getBounds().toGeoJson());
+                // Add bounds to filters
+                this.options.filters.centroid__within = JSON.stringify(
+                        this.getMap().getBounds().toGeoJson());
 
-            // Build request url
-            var url = this.options.url + '?' + $.param(this.options.filters, true);
-            this._makeJsonRequest(url, this._processFeatures);
-        },
+                // Build request url
+                var url = this.options.url + '?' + $.param(this.options.filters, true);
+                this._makeJsonRequest(url, this._processFeatures);
+            },
 
-        _makeJsonRequest: function(url, callback) {
-            var instance = this;
-            instance.getMap().fire('dataloading');
-            $.getJSON(url, function(data) {
-                // Ensure this is the layer
-                callback.apply(instance, [data,]);
-            })
-            .always(function() {
-                instance.getMap().fire('dataload');
-            });
-        },
+            _makeJsonRequest: function (url, callback) {
+                var instance = this;
+                instance.getMap().fire('dataloading');
+                $.getJSON(url, function (data) {
+                    // Ensure this is the layer
+                    callback.apply(instance, [data,]);
+                })
+                .always(function () {
+                    instance.getMap().fire('dataload');
+                });
+            },
 
-    });
-});
+        });
+    }
+);

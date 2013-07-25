7d475d24841b30ae0fca72a97453cfb11c18010e
Eric Brelsford <ebrelsford@gmail.com>
2013-07-24 23:01:24 -0400

    Only give choices to staff

--- vacant_to_vibrant/phillydata/owners/autocomplete_light_registry.py
+++ vacant_to_vibrant/phillydata/owners/autocomplete_light_registry.py
@@ -7,5 +7,12 @@ class OwnerAutocomplete(AutocompleteModelBase):
     autocomplete_js_attributes = {'placeholder': 'Owner name',}
     search_fields = ('name',)
 
+    def choices_for_request(self):
+        choices = super(OwnerAutocomplete, self).choices_for_request()
+
+        if not self.request.user.is_staff:
+            choices = choices.none()
+
+        return choices
 
 register(Owner, OwnerAutocomplete)

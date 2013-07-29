3f5dc54b141f7c6608083a70c5fdfcdcf8523f38
Eric Brelsford <ebrelsford@gmail.com>
2013-07-29 11:41:01 -0400

    Separate CityCouncilSynchronizer

--- vacant_to_vibrant/phillydata/synchronizers.py
+++ vacant_to_vibrant/phillydata/synchronizers.py
@@ -10,30 +10,6 @@ from .taxaccounts.models import TaxAccount
 logger = logging.getLogger(__name__)
 
 
-class CityCouncilSynchronizer(Synchronizer):
-    """A Synchronizer that updates city council districts for lots."""
-
-    def sync(self, data_source):
-        logger.info('Starting to synchronize city council districts.')
-        self.update_city_council_districts(count=data_source.batch_size or 1000)
-        logger.info('Finished synchronizing city council districts.')
-
-    def update_city_council_districts(self, count=1000):
-        lots = Lot.objects.filter(
-            city_council_district__isnull=True
-        ).order_by('?')
-        for lot in lots[:count]:
-            try:
-                lot.city_council_district = Boundary.objects.get(
-                    geometry__contains=lot.centroid,
-                    layer__name='City Council Districts',
-                )
-                lot.save()
-            except Exception:
-                logger.warn('Caught exception while updating city council '
-                            'district for lot %s' % lot)
-
-
 class PlanningDistrictSynchronizer(Synchronizer):
     """A Synchronizer that updates planning districts for lots."""
 

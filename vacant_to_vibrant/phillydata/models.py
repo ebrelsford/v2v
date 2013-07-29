670b199e56173f84e539aed8471cdbc1d9eeb8eb
Eric Brelsford <ebrelsford@gmail.com>
2013-07-29 10:54:31 -0400

    Use DataSource.name instead

--- vacant_to_vibrant/phillydata/models.py
+++ vacant_to_vibrant/phillydata/models.py
@@ -1,30 +1,5 @@
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
-
 from external_data_sync.models import DataSource
-
-from .synchronizers import *
 
 
 class PhillyDataSource(DataSource):
-    """
-    A DataSource that is specific to Philadelphia.
-
-    """
-    NAME_CHOICES = (
-        ('citycouncil', 'City Council'),
-        ('lilicenses', 'L&I Licenses'),
-        ('liviolations', 'L&I Violations'),
-        ('landusearea', 'Land Use Areas'),
-        ('opa', 'OPA API'),
-        ('planningdistrict', 'Planning Districts'),
-        ('praavailableproperties', 'PRA Available Properties'),
-        ('taxaccount', 'Tax Accounts'),
-        ('usecertaintyscores', 'Use Certainty Scores'),
-        ('waterdept', 'Water Department data'),
-        ('zoning', 'Zoning'),
-    )
-    name = models.CharField(_('name'),
-        max_length=50,
-        choices=NAME_CHOICES,
-    )
+    pass

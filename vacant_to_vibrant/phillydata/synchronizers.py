f971a6d95c4e848edf1bae5362f81f6b05f43268
Eric Brelsford <ebrelsford@gmail.com>
2013-07-29 11:46:10 -0400

    Separate TaxAccountSynchronizer

--- vacant_to_vibrant/phillydata/synchronizers.py
+++ vacant_to_vibrant/phillydata/synchronizers.py
@@ -4,7 +4,6 @@ from external_data_sync.synchronizers import Synchronizer
 from inplace.boundaries.models import Boundary
 
 from lots.models import Lot
-from .taxaccounts.models import TaxAccount
 
 
 logger = logging.getLogger(__name__)
@@ -32,29 +31,6 @@ class PlanningDistrictSynchronizer(Synchronizer):
             except Exception:
                 logger.warn('Caught exception while updating planning '
                             'district for lot %s' % lot)
-
-
-class TaxAccountSynchronizer(Synchronizer):
-    """A Synchronizer that updates tax account data for lots."""
-
-    def sync(self, data_source):
-        logger.info('Starting to synchronize tax account data.')
-        self.update_tax_accounts(count=data_source.batch_size)
-        logger.info('Finished synchronizing tax account data.')
-
-    def update_tax_accounts(self, count=1000):
-        lots = Lot.objects.filter(tax_account__isnull=True).order_by('?')
-        for lot in lots[:count]:
-            try:
-                lot.tax_account = TaxAccount.objects.get(
-                    property_address__icontains=lot.address_line1,
-                )
-                lot.save()
-            except TaxAccount.DoesNotExist:
-                logger.debug('Could not find tax account for lot %s' % lot)
-            except Exception:
-                logger.warn('Caught exception while getting tax account for '
-                            'lot %s' % lot)
 
 
 class UseCertaintyScoresSynchronizer(Synchronizer):
